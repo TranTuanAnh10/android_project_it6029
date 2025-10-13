98ac689e8c928b0ef083df8fc3fe3d1723806fde
Tran Tuan Anh <ttanhhh.1010@gmail.com>
2025-10-13 22:00:50 +0700

    add theme base

--- android_project/app/src/main/java/vn/haui/android_project/MainActivity.java
+++ android_project/app/src/main/java/vn/haui/android_project/MainActivity.java
@@ -1,6 +1,8 @@
 package vn.haui.android_project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import vn.haui.android_project.ui.main.SplashCreen.SplashScreen;
 
+public class MainActivity extends AppCompatActivity {
+    private FrameLayout container;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        container = findViewById(R.id.containerMain);
+    }
+
+    public void showScreen(Object screen) {
+        View view = null;
+        if (screen instanceof SplashScreen)
+            view = ((SplashScreen) screen).getView();
+//        else if (screen instanceof LoginScreen)
+//            view = ((LoginScreen) screen).getView();
+//        else if (screen instanceof HomeScreen)
+//            view = ((HomeScreen) screen).getView();
+
+        if (view != null) {
+            container.removeAllViews();
+            container.addView(view);
+        }
     }
 }
